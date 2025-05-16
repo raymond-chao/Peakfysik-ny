f9b9bdab0d46727e22dd38937924020cc88be09a
raymond-chao <raymond_chao45@hotmail.com>
2025-05-16 23:45:01 +0200

    html and css changes

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let logo = document.getElementById('logo');
+let bild = document.getElementById('bild1');
+let maintext = document.querySelector('maintext');
+let title = document.getElementById('title'); // Corrected line
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    title.style.marginTop = value * 2.5 + 'px';
+    maintext.style.marginTop = value * 2.5 + 'px';
+    bild.style.top = value * -1.5 + 'px';
+});
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.to(".nav-logo", {
+    rotation: 360,
+    duration: 2,
+  });
+  gsap.from(".nav-button", {
+    scale:1.2,
+    duration: 2,
+    ease: "bounce.out",
+  });
+});
