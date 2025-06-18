9dcc906801da6b306c755c0f09396e4a703567a6
raymond-chao <raymond_chao45@hotmail.com>
2025-06-18 20:26:19 +0200

    navbar (hamburger menu)for media fixed and small clean ups

--- js/services.js
+++ js/services.js
@@ -35,3 +35,21 @@ prevBtn.onclick = () => {
 
     slides [slideNumber].classList.add('active');
 }
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
